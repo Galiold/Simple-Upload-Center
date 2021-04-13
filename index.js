668fd076f97041c665b3670f7e8adee2fa549502
Ali Goldani <ali.goldani@ymail.com>
2021-04-13 15:12:24 +0430

    Initial upload

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const multer = require('multer')
+const path = require('path')
+let Mustache = require('mustache')
+let fs = require('fs')
+const serveIndex = require('serve-index')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.static(__dirname + '/public'))
+app.use('/uploads', serveIndex(__dirname + '/public/uploads'))
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'public/uploads/')
+    },
+
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}...`)
+})
+
+
+app.post('/upload', (req, res) => {
+    let upload = multer({
+        storage: storage
+    }).array('multiple-files')
+
+    upload(req, res, err => {
+        if (err) {
+            console.log(err)
+            res.status(400).send(err)
+        } else {
+            let view = {
+                files: req.files.map(elem => {
+                    return { name: elem.originalname, uri: encodeURI(elem.originalname), index: req.files.indexOf(elem) + 1 }
+                })
+            }
+
+            fs.readFile('public/result.html', 'utf8', (err, data) => {
+                if (err) {
+                    console.log(err)
+                    res.status(400).send('Error Occured')
+                } else {
+                    res.send(Mustache.render(data, view))
+                }
+            })
+        }
+    })
+})
