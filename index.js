199831e2d215bcae32bd300a396b5a92e8fea2f6
Ali Goldani <ali.goldani@ymail.com>
2022-01-04 09:52:14 +0330

    added password protection to configs resource

--- index.js
+++ index.js
@@ -1,20 +1,71 @@
-const express = require('express')
-const multer = require('multer')
+require('dotenv').config()
+
+const fs = require('fs')
 const path = require('path')
-let Mustache = require('mustache')
-let fs = require('fs')
+const multer = require('multer')
+const express = require('express')
+const Mustache = require('mustache')
+const session = require('express-session')
+const exec = require('child_process').exec
 const serveIndex = require('serve-index')
 const url = require("url");
-const exec = require('child_process').exec
-const e = require('express')
+
+const auth = require('./lib/auth')
 
 const app = express()
+
 const port = process.env.PORT || 3000
+const SECRET = process.env.SECRET ? process.env.SECRET : 'ETbrCY|PlBd(ig7'
 
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({
+    extended: true
+}))
 app.use(express.json())
 app.use(express.static(__dirname + '/public'))
+
+app.use(session({
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
+    secret: SECRET
+}));
+
+app.post('/authenticate/:resource', (req, res) => {
+    console.log('post auth');
+    console.log(req.body);
+    if (auth.passVerified(req.body['pass'])) {
+        let payload = {
+            message: 'User is valid'
+        }
+        req.session.auth = auth.signToken(payload)
+        console.log('Pass verified');
+        res.send({
+            'redirectURL': `/uploads/${req.params.resource}`
+        })
+    }
+    else {
+        res.status(400).send('Failed to login')
+    }
+})
+
+app.get('/uploads/:resource', (req, res, next) => {
+    if (req.params.resource === 'configs') {
+        if (auth.hasAccess(req).verify) return next()
+        else {
+            res.redirect(url.format({
+                pathname: '/auth.html',
+                query: {
+                    resource: req.params.resource
+                }
+            }))
+        }
+    } else {
+        next()
+    }
+})
+
+
+
 app.use('/uploads', serveIndex(__dirname + '/public/uploads'))
 
 const storage = multer.diskStorage({
@@ -45,7 +96,12 @@ app.post('/upload', (req, res) => {
         } else {
             let view = {
                 files: req.files.map(elem => {
-                    return { index: req.files.indexOf(elem) + 1, name: elem.originalname, uri: encodeURI(elem.originalname), size: (Math.floor(elem.size / 1024)).toString() + ' KB' }
+                    return {
+                        index: req.files.indexOf(elem) + 1,
+                        name: elem.originalname,
+                        uri: encodeURI(elem.originalname),
+                        size: (Math.floor(elem.size / 1024)).toString() + ' KB'
+                    }
                 })
             }
 
@@ -76,9 +132,11 @@ app.post('/download', (req, res) => {
             console.log(sout);
 
             let view = {
-                files: [
-                    { index: 1, name: fileName, uri: encodeURI(fileName) }
-                ]
+                files: [{
+                    index: 1,
+                    name: fileName,
+                    uri: encodeURI(fileName)
+                }]
             }
 
             fs.readFile('public/result.html', 'utf8', (err, data) => {
